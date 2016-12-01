315bef3bd77a297ca4e5e00a5690858b03964b0d
luckyharryji <xiangyu.ji@outlook.com>
2016-11-30 21:40:45 -0600

    add test to see if contant plue is constant

---
+++ Homework/H7/tests/test5/program.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "CAT.h"
+
+CATData p (int value){
+  if (value > 10){
+    return CAT_create_signed_value(5);
+  }
+
+  return CAT_create_signed_value(6);
+}
+
+CATData q1 (int valueQ){
+  return p(1);
+}
+
+
+int main (int argc, char *argv[]){
+  CATData r = q1(1);
+
+  int p = 1 + 1;
+  printf("%lld\n", CAT_get_signed_value(r));
+
+	return 0;
+}
